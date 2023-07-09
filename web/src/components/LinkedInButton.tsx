159fd2dd90b49fc76983c3cd8e4af541cb7c8dee
adminsfmeal <42289953+adminsfmeal@users.noreply.github.com>
2023-07-09 01:06:16 -0700

    update serverless function location

--- web/src/components/LinkedInButton.tsx
+++ web/src/components/LinkedInButton.tsx
@@ -7,10 +7,10 @@ const LinkedInButton = () => {
   const { logIn, isAuthenticated, currentUser, getToken } = useAuth()
 
   if (isAuthenticated) {
-    fetch('https://api.linkedin.com/v2/jobs', {
+    fetch('https://api.linkedin.com/v2/me', {
       headers: {
         Authorization:
-          'Bearer AQUlsIHNlw-_DbJ43Xnf3ZJevsolM7e4JKpnCEx6uxW8gAfQTvXsmNY8N1g_zV1L_XHo_wc4sGansYTuvDnhZA48MMG1LAntnFuLrsULW16qBh1iDkPoC1j3lccvl5IEMw00kt5qMDZs78l8IhTPFDZQB4vMLCFEWq_v-Cl2pjXt_PZ6vxdlfjQjOzQcgSj9ugl_ub9gFinsqlxZi-V9bE9Z7a8ce76R2y5TovGzFzAcI3_JGBso5xGVeLtjSl9b6zlns3p2YWfkOfmUNFS8cEjEU38z-AK30oXo2mFfW7dfVboqpiHc1uYi0io0mtZNsla-Ql_rg4EwDFXFY6KttF3XCiMzLQ',
+          'Bearer AQXNDhpO5oei6YZZTs-NWxBA2EGKDo1zo20FykSI-8w2m4_3ncaIsyvURbUYGu3J1WWDnZEmyCvgDdU_0KZVjBU5NMlBmGau-Jpj_PorcZrs8uVMocNYTxXEUc7tWHNRmoRlhHrpPsx5IIXU3R9gpAwtGaGKVa0GNWgWGlV6zj_OHonQW0x2cglWvrkl9d4BfxIFZmTNXkp0TFV1u47vFNMElLimVmuzzXdc69w1g3B7tExjM2RL4ANhwFxCCUdJyYTBDf7cviUzr37NP3MjYKEukMVojHTLDpqByfFUoR41wSp8CeB8mYbY2gk_JlqgB-8_5zCwjEuWJVhgXgNnK_hGGLuy2g',
       },
     })
       .then((resp) => resp.json())
